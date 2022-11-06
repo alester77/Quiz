69ce40f071c53892ca425e4ef2f49e534133e33c
alester77 <113203488+alester77@users.noreply.github.com>
2022-11-06 15:52:00 -0700

    working on adding elements to quiz

--- assets/script.js
+++ assets/script.js
@@ -62,3 +62,49 @@ var mark = 0;
 var index = 0;
 var record = [];
 
+
+//create visuals of the quiz
+function begin() {
+    var remove = container;
+    while(remove.hasChildNodes()){
+        remove.removeChild(remove.firstChild);
+    console.log(remove);
+    };
+
+    //create timer section (seconds)
+    var time = document.createElement("p");
+    time.textContent="Time Left"
+    var second = document.createElement("p");
+    time.appendChild(second);
+
+    //create high score section
+    var highScore = document.createElement("button");
+    viewScore.classList.add("banner", "see-score");
+    highScore.textContent= "See High Scores";
+
+    //create title
+    var title = document.createElement("h1");
+    title.textContent = "Javascript quiz"
+
+    //create starter paragraph
+    var instructions = document.createElement("h2");
+    instructions.textContent = "Do your best! This quiz is timed and wrong answer will deduct from the time you have.";
+
+    //create start button
+    var startBtn = document.createElement("button");
+    startBtn.classList.add("btn", "btn-start");
+    startBtn.textContent= "Begin Quiz";
+
+    header.appendChild(time);
+    header.appendChild(highScore);
+    container.appendChild(title);
+    container.appendChild(instructions);
+    container.appendChild(startBtn);
+
+    // Click to start
+    document.querySelector("btn-start").addEventListener("click"); //still need to add once timer function is done
+    // Click for scores
+    document.querySelector("see-score").addEventListener("click"); //still need high score function
+
+
+}
